98ef941fe3840bd25e18953fcc4d3d58963da670
ll2313673906 <3340624204@qq.com>
2020-02-06 22:20:37 +0800

    知乎-首页完善

---
+++ src/views/ColumnDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="column-detail" >
+     <div class="card" v-for="(column,index) in columns" :key="columns.index">
+         <div class="title">{{ column.title }}</div>
+         <div class="avatar"></div>
+     </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ColumnDetail",
+        data(){
+            return{
+                columns:[],
+                index: this.$route.params.index
+            }
+        },
+        created() {
+            this.axios.get('http://localhost:8080/api/columns/topColumns/' + this.index).then( res =>{
+                console.log(res)
+                this.columns = res.data.data
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .column-detail{
+        background: red;
+    }
+</style>
